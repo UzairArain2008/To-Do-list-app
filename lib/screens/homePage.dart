57ee04bca6819301a244d126c358a0a35e1165cc
Uzair Arain <142334690+UzairArain2008@users.noreply.github.com>
2026-02-08 23:29:50 +0500

    initial commit

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -89,24 +89,29 @@ class Tasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: () {
+        print('object');
+      },
       child: Container(
         width: double.infinity,
-        height: 100,
+        height: 120,
         padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
         decoration: BoxDecoration(
           color: Colors.white,
           // border: Border.all(color: Color(0xffcccccc), width: 3),
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(20),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            Text('Study Time', style: TextTheme.of(context).titleMedium),
-            Text(
-              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Color(0xff000000)),
+            GestureDetector(
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(color: Color(0XFFcccccc), width: 3),
+                ),
+              ),
             ),
           ],
         ),
