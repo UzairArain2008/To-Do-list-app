31ee4bfd11c745b04d8ec3cf9418ddbd48e200dd
Uzair Arain <142334690+UzairArain2008@users.noreply.github.com>
2026-02-19 22:42:37 +0500

    initial commit

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -11,7 +11,7 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   String selectedTag = 'All';
 
-  final List<String> allTags = ['All', 'study', 'work', 'personal'];
+  final List<String> allTags = ['All', 'Study', 'Work', 'Personal'];
 
   final List<Map<String, dynamic>> tasks = [
     {
@@ -44,97 +44,208 @@ class _HomepageState extends State<Homepage> {
     },
   ];
 
-  void showAddTaskDialog() {
+  void showAddTaskBottomSheet() {
     String title = '';
     String description = '';
-    String tag = 'study';
+    String tag = 'Study';
+    DateTime? selectedDate;
+    TimeOfDay? selectedTime;
 
-    showDialog(
+    showModalBottomSheet(
       context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+      ),
       builder: (context) {
-        return AlertDialog(
-          backgroundColor: Color(0xffffffff),
-          titleTextStyle: TextStyle(color: Color(0xff000000)),
-          title: Text('Add New Task'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Color(0xff000000),
-                  label: Text(
-                    'Task Title',
-                    style: TextStyle(color: Color(0xff000000)),
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 16,
+            left: 16,
+            right: 16,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Add New Task',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              TextField(),
-              DropdownButtonFormField<String>(
-                value: tag,
-                items: allTags
-                    .where((t) => t != 'all')
-                    .map((t) => DropdownMenuItem(value: t, child: Text(t)))
-                    .toList(),
-                onChanged: (value) {
-                  if (value != null) tag = value;
-                },
-              ),
-            ],
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    label: Text(
+                      'Task Title',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    ),
+                  ),
+                  onChanged: (value) => title = value,
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    label: Text(
+                      'Task Description',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    ),
+                  ),
+                  onChanged: (value) => description = value,
+                ),
+                SizedBox(height: 16),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: 'Tag',
+                    labelStyle: TextStyle(color: Colors.black),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(width: 2, color: Colors.grey),
+                    ),
+                  ),
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  items: allTags
+                      .where((t) => t != 'All')
+                      .map((t) => DropdownMenuItem(value: t, child: Text(t)))
+                      .toList(),
+                  onChanged: (value) {
+                    if (value != null) tag = value;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.calendar_today, color: Colors.blue),
+                    label: Text(
+                      'Select Date',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    ),
+                  ),
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate ?? DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        selectedDate = pickedDate;
+                      });
+                    }
+                  },
+                  controller: TextEditingController(
+                    text: selectedDate != null
+                        ? "${selectedDate!.day}-${selectedDate!.month}-${selectedDate!.year}"
+                        : '',
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.access_time, color: Colors.blue),
+                    label: Text(
+                      'Select Time',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    ),
+                  ),
+                  onTap: () async {
+                    TimeOfDay? pickedTime = await showTimePicker(
+                      context: context,
+                      initialTime: selectedTime ?? TimeOfDay.now(),
+                    );
+                    if (pickedTime != null) {
+                      setState(() {
+                        selectedTime = pickedTime;
+                      });
+                    }
+                  },
+                  controller: TextEditingController(
+                    text: selectedTime != null
+                        ? "${selectedTime!.hour.toString().padLeft(2, '0')}:${selectedTime!.minute.toString().padLeft(2, '0')}"
+                        : '',
+                  ),
+                ),
+                SizedBox(height: 24),
+                // Actions
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (title.isNotEmpty &&
+                            description.isNotEmpty &&
+                            selectedDate != null &&
+                            selectedTime != null) {
+                          setState(() {
+                            tasks.add({
+                              'tag': tag,
+                              'title': title,
+                              'description': description,
+                              'dateTime': DateTime(
+                                selectedDate!.year,
+                                selectedDate!.month,
+                                selectedDate!.day,
+                                selectedTime!.hour,
+                                selectedTime!.minute,
+                              ),
+                              'isDone': false,
+                            });
+                          });
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text('Save Task'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
           ),
-          // content: SingleChildScrollView(
-          //   child: Column(
-          //     mainAxisSize: MainAxisSize.min,
-          //     children: [
-          //       TextField(
-          //         decoration: const InputDecoration(
-          //           labelText: 'Title',
-          //           fillColor: Color(0xff000000),
-          //         ),
-          //         onChanged: (value) => title = value,
-          //       ),
-          //       TextField(
-          //         decoration: const InputDecoration(labelText: 'Description'),
-          //         onChanged: (value) => description = value,
-          //       ),
-          //       DropdownButtonFormField<String>(
-          //         value: tag,
-          //         decoration: const InputDecoration(labelText: 'Tag'),
-          //         items: allTags
-          //             .where((t) => t != 'All')
-          //             .map((t) => DropdownMenuItem(value: t, child: Text(t)))
-          //             .toList(),
-          //         onChanged: (value) {
-          //           if (value != null) tag = value;
-          //         },
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (title.isNotEmpty && description.isNotEmpty) {
-                  setState(() {
-                    tasks.add({
-                      'tag': tag,
-                      'title': title,
-                      'description': description,
-                      'dateTime': DateTime.now(),
-                      'isDone': false,
-                    });
-                  });
-                  Navigator.of(context).pop();
-                }
-              },
-              child: const Text('Save Task'),
-            ),
-          ],
         );
       },
     );
@@ -191,7 +302,7 @@ class _HomepageState extends State<Homepage> {
                     color: const Color(0xFF94A3B8),
                   ),
                   child: IconButton(
-                    onPressed: showAddTaskDialog,
+                    onPressed: showAddTaskBottomSheet,
                     icon: const Icon(Icons.add, color: Color(0xFF222222)),
                   ),
                 ),
